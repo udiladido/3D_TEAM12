73cdc7be5229c3403313ca0bdb3cc10162727773
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-11-18 20:25:54 +0900

    [Fix] 플레이어 캐릭터 땅 체크 레이어 변경, GameManager 베이스만..

---
+++ Assets/Scripts/Managers/Content/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : IManager
+{
+    public void Init()
+    {
+        // 게임시작하기 전처리
+        
+    }
+    public void Clear()
+    {
+        // 데이터는 필요가 없음
+    }
+
+    public void GameStart()
+    {
+        // 실제로 게임을 시작하는 함수
+    }
+
+    public void GameOver()
+    {
+        // 실제로 게임이 종료되었을때 함수
+    }
+}
